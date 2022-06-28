a198c88bd6a7ce1baa4ab1a4db3684374e3381bb
a-bigelow <adam@adambigelow.com>
2022-06-28 16:13:16 -0400

    chore: bumping CDK to latest so this example is relevant

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.29.0',
   defaultReleaseBranch: 'main',
   name: 'example-cdk-pipeline',
   deps: ['cdk-nag'],
