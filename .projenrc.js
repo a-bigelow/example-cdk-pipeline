3fee443cd0ac7a3285ab820a64100444b27322a7
a-bigelow <adam@adambigelow.com>
2022-08-05 08:34:01 -0400

    chore: disabling deps upgrades

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.29.0',
   defaultReleaseBranch: 'main',
   name: 'example-cdk-pipeline',
+  depsUpgrade: false,
   deps: ['cdk-nag'],
   gitignore: ['.idea'],
 });
